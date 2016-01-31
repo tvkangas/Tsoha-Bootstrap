e871fc055b6b309e9e9d592256301b8dc28f9daa
Tuukka <tuukka@tuukka-VirtualBox.(none)>
2016-01-31 16:46:51 +0200

    Sovelluksen laajentamista

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,31 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  View::make('suunnitelmat/etusivu.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+    
+    public static function login() {
+        View::make('suunnitelmat/etusivu.html');
+    }
+    
+    public static function radat() {
+        View::make('suunnitelmat/ratojenlistaus.html');
+    }
+    
+    public static function tuloslisays() {
+        View::make('suunnitelmat/tuloksenlisays.html');
+    }
+    
+    public static function tuloslistaus() {
+        View::make('suunnitelmat/tuloksienlistaus.html');
+    }
+    
+    public static function radanlisays() {
+        View::make('suunnitelmat/radanlisays.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,23 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/etusivu', function() {
+    HelloWorldController::login();
+  });  
+  
+  $routes->get('/radat', function() {
+    HelloWorldController::radat();
+  });  
+  
+  $routes->get('/tuloslisays', function() {
+    HelloWorldController::tuloslisays();
+  }); 
+  
+  $routes->get('/tuloslistaus', function() {
+    HelloWorldController::tuloslistaus();
+  }); 
+  
+  $routes->get('/radanlisays', function() {
+    HelloWorldController::radanlisays();
+  }); 

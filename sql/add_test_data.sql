7756d9c3884bd1b80b4627f0af93f1ab0470429f
Tuukka <tuukka@tuukka-VirtualBox.(none)>
2016-01-31 16:53:31 +0200

    korjauksia testidataan

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -12,6 +12,4 @@ INSERT INTO RadanIhannetulokset (rata_id, vayla1, vayla2, vayla3, vayla4, vayla5
 INSERT INTO RadanIhannetulokset (rata_id, vayla1, vayla2, vayla3, vayla4, vayla5, vayla6, vayla7, vayla8, vayla9, vayla10, vayla11, vayla12, vayla13, vayla14, vayla15, vayla16, vayla17, vayla18) VALUES ( (SELECT id FROM Rata WHERE nimi='Munkkiniemi'), 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 INSERT INTO RadanIhannetulokset (rata_id, vayla1, vayla2, vayla3, vayla4, vayla5, vayla6, vayla7, vayla8, vayla9, vayla10, vayla11, vayla12, vayla13, vayla14, vayla15, vayla16, vayla17, vayla18) VALUES ( (SELECT id FROM Rata WHERE nimi='Valko'), 4, 3, 3, 3, 3, 4, 3, 3, 3, 3, 3, 4, 3, 3, 3, 3, 4, 3);
 
---Lisätään parit tulokset
-INSERT INTO TuloksetVaylittain (rata_id, vayla1, vayla2, vayla3, vayla4, vayla5, vayla6, vayla7, vayla8, vayla9, vayla10, vayla11, vayla12, vayla13, vayla14, vayla15, vayla16, vayla17, vayla18) VALUES ( (SELECT id FROM Rata WHERE nimi='Siltamaki'), 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2);
-INSERT INTO RadanIhannetulokset (rata_id, vayla1, vayla2, vayla3, vayla4, vayla5, vayla6, vayla7, vayla8, vayla9, vayla10, vayla11, vayla12, vayla13, vayla14, vayla15, vayla16, vayla17, vayla18) VALUES ( (SELECT id FROM Rata WHERE nimi='Siltamaki'), 2, 3, 3, 5, 3, 3, 3, 3, 3, 3, 1, 3, 4, 3, 3, 3, 3, 2);
+--Tuloksien lisääminen ei onnistunut, jatkan työstämistä
